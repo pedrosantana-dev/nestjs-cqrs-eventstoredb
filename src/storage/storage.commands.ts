d41db2e393a2fe91faff0ccb7753d99fd06a85fd
Pedro Santana <pedromanu27@gmail.com>
2024-10-25 00:53:17 -0300

    feat: :sparkles: add command classes

---
+++ src/storage/storage.commands.ts
@@ -0,0 +1,16 @@
+import { ICommand } from '@nestjs/cqrs';
+
+export class RegisterStorageUnitCommand implements ICommand {
+    constructor(
+        public readonly aggregateId: string,
+        public readonly capacity: string,
+    ) {}
+}
+
+export class DisableStorageUnitCommand implements ICommand {
+    constructor(public readonly aggregateId: string) {}
+}
+
+export class EnableStorageUnitCommand implements ICommand {
+    constructor(public readonly aggregateId: string) {}
+}
